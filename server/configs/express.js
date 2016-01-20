b5bb94520b4a084b521527fe453dfb2d9b2bd74f
Yakira C. Bristol <hello@yakirac.me>
2016-01-19 21:49:56 -0500

    Update Cross Domain

--- server/configs/express.js
+++ server/configs/express.js
@@ -102,7 +102,8 @@
 		app.use(cors());
 		app.use(function(req, res, next) {
 			// Website you wish to allow to connect
-			res.set("Access-Control-Allow-Origin", "*");
+			//res.set("Access-Control-Allow-Origin", "*");
+			res.set("Access-Control-Allow-Origin", "https://plaid.com/careers/submit/");
 			// Request methods you wish to allow
 			res.set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
 			// Request headers you wish to allow
